99057dfebb253538a1706f6c5de437025f03b727
AiliS-pro <chopper.man.015@gmail.com>
2020-09-15 18:58:23 +0900

    display from database

--- app/controllers/aromas_controller.rb
+++ app/controllers/aromas_controller.rb
@@ -1,6 +1,11 @@
 class AromasController < ApplicationController
 
   def index
+    @aromas = Aroma.all
   end
-  
+
+  private
+  def aroma_params
+    params.require(:aroma).permit(:name, :family_name, :fragrance_note, :fragrance_type, :explain_text, :img)
+  end
 end
