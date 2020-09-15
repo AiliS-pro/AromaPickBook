93bc4c41a70991fef060fb3743270566921f0c11
AiliS-pro <chopper.man.015@gmail.com>
2020-09-15 21:49:36 +0900

    new function preparation
    create new link

--- app/controllers/aromas_controller.rb
+++ app/controllers/aromas_controller.rb
@@ -4,6 +4,10 @@ class AromasController < ApplicationController
     @aromas = Aroma.all
   end
 
+  def new
+    @aroma = Aroma.new
+  end
+
   private
   def aroma_params
     params.require(:aroma).permit(:name, :family_name, :fragrance_note, :fragrance_type, :explain_text, :img)
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root "aromas#index"
+  resources :aromas
 end
